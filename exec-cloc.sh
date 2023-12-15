ab2feb2fb402aabc430840270da2bbb093960275
Matthew Green <greenmatthew07@gmail.com>
2023-12-14 21:42:54 -0600

    Updating script that uses cloc to count the lines of code I have done for HarvestHavoc and Velecs combined.

--- exec-cloc.sh
+++ exec-cloc.sh
@@ -5,11 +5,20 @@ cloc \
 --ignored=ignored.txt \
 --force-lang="C++",cpp.DISABLED \
 --force-lang="C/C++ Header",h.DISABLED \
-harvest_havoc/ \
-velecs/ \
-libs/CMakeLists.txt \
 CMakeLists.txt \
-*.md
+*.md \
+Makefile \
+src/ \
+include/ \
+libs/CMakeLists.txt \
+libs/velecs/CMakeLists.txt \
+libs/velecs/src \
+libs/velecs/include \
+libs/velecs/libs/CMakeLists.txt \
+libs/velecs/assets/CMakeLists.txt \
+libs/velecs/assets/shaders/ \
+libs/velecs/assets/shaders/CMakeLists.txt \
+libs/velecs/assets/meshes/CMakeLists.txt
 
 # Check if ignored.txt was created and is not empty
 if [ -s ignored.txt ]; then
