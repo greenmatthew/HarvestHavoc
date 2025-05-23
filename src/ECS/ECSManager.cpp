af7b5e25b465429c54706614f510b80586899e40
Matthew Green <greenmatthew07@gmail.com>
2025-05-23 16:43:54 -0500

    Started swapping to new velecs-X lib hierarchy.

--- src/ECS/ECSManager.cpp
+++ src/ECS/ECSManager.cpp
@@ -1,9 +1,9 @@
 /// @file    ECSManager.cpp
 /// @author  Matthew Green
 /// @date    10/30/2023 16:17:43
-/// 
+///
 /// @section LICENSE
-/// 
+///
 /// Copyright (c) 2023 Matthew Green - All rights reserved
 /// Unauthorized copying of this file, via any medium is strictly prohibited
 /// Proprietary and confidential
@@ -20,60 +20,59 @@
 
 using namespace velecs;
 
-namespace hh {
-
-// Public Fields
-
-// Constructors and Destructors
-
-ECSManager::ECSManager(velecs::VelECSEngine& engine)
-    : IECSManager(engine)
+namespace hh
 {
-    // Required for ability to shutdown the application
-    ecs.import<InputECSModule>();
 
-    ecs.import<EnvironmentECSModule>();
-    
-    ecs.import<PlayerECSModule>();
+    // Public Fields
 
-    ecs.import<MenusECSModule>();
-}
+    // Constructors and Destructors
 
-// Public Methods
-
-void ECSManager::Init()
-{
-    
-}
-
-void ECSManager::Cleanup()
-{
-    
-}
-
-bool ECSManager::GetIsQuitting() const
-{
-    flecs::entity inputEntity = ecs.singleton<Input>();
-    if (inputEntity == flecs::entity::null())
+    ECSManager::ECSManager(velecs::VelECSEngine &engine)
+        : IECSManager(engine)
     {
-        throw std::runtime_error("Missing import of " + std::string(typeid(InputECSModule).name()) + ". Without it, the application cannot close properly.");
+        // Required for ability to shutdown the application
+        ecs.import <InputECSModule>();
+
+        ecs.import <EnvironmentECSModule>();
+
+        ecs.import <PlayerECSModule>();
+
+        ecs.import <MenusECSModule>();
     }
 
-    const Input * const input = inputEntity.get<Input>();
-    if (input == nullptr)
+    // Public Methods
+
+    void ECSManager::Init()
     {
-        throw std::runtime_error("Missing import of " + std::string(typeid(InputECSModule).name()) + ". Without it, the application cannot close properly.");
     }
 
-    return input->isQuitting;
-}
+    void ECSManager::Cleanup()
+    {
+    }
 
-// Protected Fields
+    bool ECSManager::GetIsQuitting() const
+    {
+        flecs::entity inputEntity = ecs.singleton<Input>();
+        if (inputEntity == flecs::entity::null())
+        {
+            throw std::runtime_error("Missing import of " + std::string(typeid(InputECSModule).name()) + ". Without it, the application cannot close properly.");
+        }
 
-// Protected Methods
+        const Input *const input = inputEntity.get<Input>();
+        if (input == nullptr)
+        {
+            throw std::runtime_error("Missing import of " + std::string(typeid(InputECSModule).name()) + ". Without it, the application cannot close properly.");
+        }
 
-// Private Fields
+        return input->isQuitting;
+    }
 
-// Private Methods
+    // Protected Fields
+
+    // Protected Methods
+
+    // Private Fields
+
+    // Private Methods
 
 } // namespace hh
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,23 +1,34 @@
 /// @file    Main.cpp
 /// @author  Matthew Green
 /// @date    10/15/2023 15:30:21
-/// 
+///
 /// @section LICENSE
-/// 
+///
 /// Copyright (c) 2023 Matthew Green - All rights reserved
 /// Unauthorized copying of this file, via any medium is strictly prohibited
 /// Proprietary and confidential
 
-#include <velecs/VelECSEngine.h>
+// #include <velecs/VelECSEngine.h>
 
-#include "ECS/ECSManager.h"
+// #include "ECS/ECSManager.h"
 
-int main(int argc, char* argv[])
+#include "velecs/engine2/Engine.hpp"
+
+int main(int argc, char *argv[])
 {
-    auto& engine = velecs::VelECSEngine{};
+    // auto &engine = velecs::VelECSEngine{};
 
-    engine.SetECS(std::make_unique<hh::ECSManager>(engine))
-        .Run();
+    // engine.SetECS(std::make_unique<hh::ECSManager>(engine))
+    //     .Run();
 
-    return 0;
+    // return 0;
+
+    auto engine = velecs::engine2::Engine::Create()
+        .SetTitle("Harvest Havoc")
+        .Init()
+        .Run()
+        .Cleanup()
+        ;
+    
+    return EXIT_SUCCESS;
 }
