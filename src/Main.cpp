059ed0c21dd470fc35dc3ae6c4ebd361c7927c43
Matthew Green <greenmatthew07@gmail.com>
2025-09-23 16:02:32 -0500

    Removed doc comment header from code as well as Enums field at top of header files.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,13 +1,3 @@
-/// @file    Main.cpp
-/// @author  Matthew Green
-/// @date    10/15/2023 15:30:21
-///
-/// @section LICENSE
-///
-/// Copyright (c) 2023 Matthew Green - All rights reserved
-/// Unauthorized copying of this file, via any medium is strictly prohibited
-/// Proprietary and confidential
-
 #include <velecs/engine/Engine.hpp>
 using namespace velecs::engine;
 
